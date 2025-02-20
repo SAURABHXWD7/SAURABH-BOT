4493c2623e7609096b8314e659f0d1a57b64729a
SAURABHXWD7 <165843437+SAURABHXWD7@users.noreply.github.com>
2025-02-20 17:46:23 +0530

    Update S.js

--- models/commands/S.js
+++ models/commands/S.js
@@ -3,25 +3,33 @@ module.exports.config = {
   name: "admin2",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩", 
+  credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
   description: "hihihihi",
   commandCategory: "no prefix",
   usages: "admin",
-    cooldowns: 5, 
+  cooldowns: 5,
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
-  if (event.body.indexOf("SAURABH")==0 || event.body.indexOf("Saurabh")==0 || event.body.indexOf("saurabh")==0 || event.body.indexOf("Babu")==0) {
-    var msg = {
+  if (
+    event.body.indexOf("SAURABH") == 0 ||
+    event.body.indexOf("Saurabh") == 0 ||
+    event.body.indexOf("saurabh") == 0 ||
+    event.body.indexOf("Babu") == 0
+  ) {
+    const imagePath = __dirname + "/noprefix/S.png"; // Verify this path
+    try {
+      var msg = {
         body: "🫅 𝗦𝗔𝗨𝗥𝗔𝗕𝗛 𝗣𝗥𝗢𝗙𝗜𝗟𝗘 🫅",
-        attachment: 
-fs.createReadStream(__dirname + `/noprefix/S.png`)
-      }
+        attachment: fs.createReadStream(imagePath),
+      };
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🫅", event.messageID, (err) => {}, true)
+      api.setMessageReaction("🫅", event.messageID, (err) => {}, true);
+    } catch (error) {
+      console.error("Error reading image:", error);
+      api.sendMessage("Error: Could not display profile.", threadID, messageID); //Send error to chat.
     }
   }
-  module.exports.run = function({ api, event, client, __GLOBAL }) {
-
-  }
+};
+module.exports.run = function({ api, event, client, __GLOBAL }) {};
