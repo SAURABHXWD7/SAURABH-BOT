e3bd1cb9a8fc2a5d3b63d72cbd5c5328860ae00d
SAURABHXWD7 <165843437+SAURABHXWD7@users.noreply.github.com>
2025-02-22 22:24:03 +0530

    Update wlc.js

--- models/commands/wlc.js
+++ models/commands/wlc.js
@@ -1,27 +1,28 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "welcome",
-  version: "1.0.1",
-  permssion: 0,
-  credits: "𝐒𝐀𝐔𝐑𝐀𝐁𝐇 𝐓𝐇𝐀𝐊𝐔𝐑", 
-  prefix : true,
-  description: "noprefix gif",
-  category: "no prefix",
-  usages: "bye",
-  cooldowns: 5, 
+	name: "Radhe-radhe",
+    version: "1.1.1",
+	hasPermssion: 0,
+	credits: "SAURABH THAKUR", 
+	description: "THIS BOT IS MR SAURABH THAKUR",
+	commandCategory: "no prefix",
+    cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-  var { threadID, messageID } = event;
-  if (event.body.indexOf("welcome")==0 || event.body.indexOf("Welcome")==0 || event.body.indexOf("WELCOME")==0 || event.body.indexOf("wel")==0 || event.body.indexOf("wl")==0 || event.body.indexOf("wlc")==0) {
-    var msg = {
-        body: "𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐇𝐎 𝐆𝐘𝐀 𝐉𝐈 🙂🖐️",
-        attachment: fs.createReadStream(__dirname + `/noprefix/wlc.mp3`)
-      }
-      api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("❤️‍🩹", event.messageID, (err) => {}, true)
-    }
-  }
-  module.exports.run = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("wel") ||
+     react.includes("Welcome") || react.includes("Wlc") || react.includes("welcome") ||
+react.includes("wlc") ||
+react.includes("WELCOME")) {
+		var msg = {
+				body: `𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐇𝐎 𝐆𝐘𝐀 𝐉𝐈 🙂`,attachment: fs.createReadStream(__dirname + `/noprefix/wlc.mp3`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🙈", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
   }
